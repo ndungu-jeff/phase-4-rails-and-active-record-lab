36cb54f6b2842d0fec4946903573497ff39fb6df
jeff ndungu <ndungujeff.ke@gmail.com>
2023-03-14 10:19:16 +0300

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'John', last_name: 'Kimani', grade: 85)
+Student.create!(first_name: 'Jack', last_name: 'Ndungu', grade: 78)
+Student.create!(first_name: 'Justin', last_name: 'Kinuthia', grade: 90)
+
+# first_student = Student.create!(first_name: 'Joseph', last_name: "Kimaiyo", grade: 90)
+# Student1 = Student.create!(first_name: 'Joseph', last_name: "Kimaiyo", grade: 90)
+# student1 = Student.create!(first_name: 'Joseph', last_name: "Kimaiyo", grade: 90)
